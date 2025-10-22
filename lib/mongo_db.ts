9ddf045f00917d4bb283ce0a18b0229d8430dd04
kshivvu <kshivvu@gmail.com>
2025-10-22 18:34:37 +0530

    fixed db serverless

--- lib/mongo_db.ts
+++ lib/mongo_db.ts
@@ -1,20 +1,36 @@
 import mongoose from "mongoose";
 
-const connectDb = async (): Promise<void> => {
-  try {
-    const mongo_URI = process.env.MONGO_URI;
-    if (!mongo_URI) throw new Error("MONGO_URI environment variable is not defined");
+declare global {
+  // prevent multiple mongoose connections in dev
+  var mongooseCache: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+}
 
-    await mongoose.connect(mongo_URI);
-    console.log("✅ MongoDB Connected");
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error(`❌ MongoDB Connection Error: ${error.message}`);
-    } else {
-      console.error("❌ Unknown error occurred while connecting to MongoDB", error);
-    }
-    process.exit(1);
+const connectDb = async (): Promise<typeof mongoose> => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI environment variable is not defined");
   }
+
+  const mongo_URI = process.env.MONGO_URI;
+
+  if (global.mongooseCache?.conn) {
+    return global.mongooseCache.conn;
+  }
+
+  if (!global.mongooseCache) {
+    global.mongooseCache = { conn: null, promise: null };
+  }
+
+  if (!global.mongooseCache.promise) {
+    global.mongooseCache.promise = mongoose
+      .connect(mongo_URI)
+      .then((mongooseInstance) => {
+        return mongooseInstance;
+      });
+  }
+
+  global.mongooseCache.conn = await global.mongooseCache.promise;
+  console.log("✅ MongoDB Connected");
+  return global.mongooseCache.conn;
 };
 
 export default connectDb;
