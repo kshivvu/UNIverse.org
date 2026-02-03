fe4e8d332581b2127a96a0f3dbe912290228b83d
kshivvu <kshivvu@gmail.com>
2026-02-04 03:39:53 +0530

    prisma singleton done

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,6 +12,15 @@
 
 
 // src/lib/prisma.ts
+// lib/prisma.ts
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client"
 
-export const prisma = new PrismaClient()
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
